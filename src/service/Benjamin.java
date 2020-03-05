1c0b6e560980fc6fe8702f31a76eebeee182ab40
Kodzo <zeushias89@gmail.com>
2020-03-05 14:46:13 +0100

    commit projet interface1

---
+++ src/service/Benjamin.java
@@ -0,0 +1,12 @@
+package service;
+
+public class Benjamin {
+
+	public void courir() {
+		System.out.println("Benjamin sait courir");
+	}
+
+	public void nager() {
+		System.out.println("Benjamin sait nager");
+	}
+}
---
+++ src/service/Fatimata.java
@@ -0,0 +1,12 @@
+package service;
+
+public class Fatimata {
+
+	public void courir() {
+		System.out.println("Fatimata sait courir");
+	}
+
+	public void nager() {
+		System.out.println("Fatimata sait nager");
+	}
+}
